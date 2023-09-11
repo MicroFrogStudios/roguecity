d2f41cb1d200df3dd8aefbaad80fcaef0f3ad04d
meskion <manufdh@hotmail.com>
2023-09-11 14:01:01 +0200

    p2. map, tiles, entities and refactoring

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import tcod
 
-from actions import EscapeAction, MovementAction
+from map.game_map import GameMap
 from inputHandlers import EventHandler
-
+from classes.entity import Entity
+from engine import Engine
 
 def main():
     screen_width = 80
     screen_height = 50
-     
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "tilesets/basic.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -17,33 +18,27 @@ def main():
 
     event_handler = EventHandler()
 
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "A", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+
+    engine = Engine(entities=entities,event_handler=event_handler,player=player,game_map=game_map)
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Rogue City project",
         vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-
-            context.present(root_console)
-
-            root_console.clear()
+            engine.render(console=root_console,context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
             
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
 
 
 if __name__ == "__main__":
