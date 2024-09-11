1f1a177285f2de0988008ab57f18623e11cea2c6
meskion <manufdh@hotmail.com>
2024-09-11 23:12:35 +0200

    skeleton of menus

--- interface/navigable_menu.py
+++ interface/navigable_menu.py
@@ -1,8 +1,11 @@
 from typing import TYPE_CHECKING
 
+
+
 if TYPE_CHECKING:
     from tcod.console import Console
     from engine import Engine
+    from interface.button import Button
 
 class Tab:
     
@@ -16,7 +19,44 @@ class TabContainer:
     tabs: list[Tab]
 
 
-class BaseMenu():
+class BaseMenu:
     
-    def render(console: Console, engine: Engine) -> None:
+    def __init__(self, submenus) -> None:
+        self.submenus : list[SubMenu] = submenus
+        self.submenu_cursor : int = 0
+
+    def render(self,console: Console, engine: Engine) -> None:
+        for sub in self.submenus:
+            sub.render(console,engine)
+    
+
+
+## menus contains submenus, submenus can be
+#  an entity with their interactables(that can be clicked or navigated)
+#  or a single column with only info
+#  or an setting that can be changed clicking buttons
+
+# might need to tell submenus form menu their root position so it nows where to move everything in it
+
+class SubMenu:
+
+    def render(self,console: Console, engine: Engine) -> None:
         raise NotImplementedError()
+
+class SingleOptionSubMenu(SubMenu):
+
+    def __init__(self, button : Button) -> None:
+        self.button = button
+
+    def render(self,console: Console, engine: Engine) -> None:
+        self.button.render(console,engine)
+
+class MultiOptionSubMenu(SubMenu):
+
+    def __init__(self,title , buttons: list[Button]) -> None:
+        self.title = title
+        self.buttons = buttons
+        
+    def render(self,console: Console, engine: Engine) -> None:
+        for b in self.buttons:
+            b.render(console,engine) # ¿?
